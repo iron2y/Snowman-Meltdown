f196cc9d5ce1e069db5753852e79c21b01a7e3d1
Ronny Borries <ronny.borries@gmail.com>
2025-07-16 16:35:14 +0200

    Added functions create_word_hint and display_game_stat to snowman.py

--- snowman.py
+++ snowman.py
@@ -41,23 +41,52 @@ def get_random_word():
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
+def create_word_hint(secret_word, guessed_letters):
+    """Creates a word hint based on the secret word."""
+    if guessed_letters:
+        hidden_word = ""
+        for char in secret_word:
+            if char in guessed_letters:
+                hidden_word += char
+            else:
+                hidden_word += "_"
+    else:
+        hidden_word = "_" * len(secret_word)
+    return " ".join(hidden_word)
+
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    """Displays the current game state."""
+    print(STAGES[mistakes])
+    hidden_word = create_word_hint(secret_word, guessed_letters)
+    if mistakes == MAX_NUMBER_OF_MISTAKES:
+        print(f"Game Over! The word was: {secret_word}")
+        return -1
+    if hidden_word.replace(" ", "") == secret_word:
+        print ("You won!")
+        return -1
+    print(f"Word: {hidden_word}")
+    return 0
+
+
 def play_game():
     secret_word = get_random_word()
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
     mistakes = 0
+    guessed_letters = set()
     while True:
-        guess = input("Guess a letter: ").lower()
+        display_game_state(mistakes, secret_word, guessed_letters)
+        guess = input("\nGuess a letter: ").lower()
         print("You guessed:", guess)
-        if guess in secret_word:
-            print("snowman_full")
-        else:
-            print("snwoman_reduced")
+        guessed_letters.add(guess)
+        if guess not in secret_word:
             mistakes += 1
-            if mistakes == MAX_NUMBER_OF_MISTAKES:
-                print(f"Game Over! The word was: {secret_word}")
-                break
+        game_state = display_game_state(mistakes, secret_word, guessed_letters)
+        if game_state == -1:
+            break
+
 
 
 if __name__ == "__main__":
