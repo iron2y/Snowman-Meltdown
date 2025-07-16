919611dc9eba7fe52ee462212574fa6b8a50ca59
Ronny Borries <ronny.borries@gmail.com>
2025-07-16 18:45:02 +0200

    Added new stages fpr smoother melting in ascii_art.py

--- ascii_art.py
+++ ascii_art.py
@@ -1,29 +1,50 @@
 # Snowman ASCII Art stages
 STAGES = [
-     # Stage 0: Full snowman
-     """
+    # Stage 0: Full snowman
+    """
       ___  
      /___\\ 
      (o o) 
      ( : ) 
      ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
+    """,
+    # Stage 1: Bottom part starts melting
+    """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : )
+     *°°°* 
+    """,
+    # Stage 2: Bottom part melted
+    """
       ___  
      /___\\ 
      (o o) 
      ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
+    """,
+    # Stage 3: Middle part starts melting
+    """
       ___  
      /___\\ 
      (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
+     *°°°* 
+    """,
+    # Stage 4: Only the head remains
+    """
       ___  
      /___\\ 
-     """
+     (o o) 
+    """,
+    # Stage 5: Head starts melting
+    """
+      ___  
+     /___\\ 
+     *°°°*  
+    """,
+     # Stage 6: Snowman completely melted
+    """
+      ___  
+     /___\\ 
+    """
  ]
